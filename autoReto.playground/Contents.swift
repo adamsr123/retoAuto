e0ac50d74b2453f39ee9c8b95f4b0c69f5ab2045
Adam Segall Rawet <Adam@imac-de-dm.local>
2016-01-28 21:21:15 +0200

    working

--- autoReto.playground/Contents.swift
+++ autoReto.playground/Contents.swift
@@ -1,4 +1,63 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-//hahahaha working?
+
+enum Velocidades : Int
+{
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init(velocidadInicial : Velocidades)
+    {
+        self = velocidadInicial
+    }
+}
+
+class Auto {
+    
+    var velocidad : Velocidades
+    
+    init()
+    {
+        self.velocidad = Velocidades(velocidadInicial: .Apagado)
+    }
+    
+    func cambioDeVelocidad() -> (actual : Int, velocidadEnCadena: String)
+    {
+        var cadena : String
+        var actual : Int
+        if velocidad.rawValue == 0
+        {
+            self.velocidad = .VelocidadBaja
+            cadena = "Velocidad baja."
+            actual = 20
+        }
+        else if velocidad.rawValue == 20
+        {
+            self.velocidad = .VelocidadMedia
+            cadena = "Velocidad media."
+            actual = 50
+        }
+        else if velocidad.rawValue == 50
+        {
+            self.velocidad = .VelocidadAlta
+            cadena = "Velocidad alta."
+            actual = 120
+        }
+        else
+        {
+            self.velocidad = .VelocidadMedia
+            cadena = "Velocidad media."
+            actual = 50
+        }
+        return (actual, cadena)
+    }
+    
+}
+
+var auto = Auto()
+print("\(auto.velocidad.rawValue), \(auto.velocidad)")
+for i in 1...20
+{
+    var tupla = auto.cambioDeVelocidad()
+    print("\(tupla.actual),\(tupla.velocidadEnCadena)")
+}
